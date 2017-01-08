e0d035e6a7f44061fb8f5d0fcd30c42722118a85
liangxieq <liangxiegame@gmail.com>
2017-01-08 22:31:02 +0800

    更新文档

--- Assets/_Script/UI/UIStartPageComponents.cs
+++ Assets/_Script/UI/UIStartPageComponents.cs
@@ -9,12 +9,15 @@ public class UIStartPageComponents : IUIComponents
 	public void InitUIComponents()
 	{
 		Image_Image = QUIManager.Instance.Get<UIStartPage>("Image").GetComponent<Image>();
+		Button_Button = QUIManager.Instance.Get<UIStartPage>("Button").GetComponent<Button>();
 	}
 
 	public void Clear()
 	{
 		Image_Image = null;
+		Button_Button = null;
 	}
 
 	public Image Image_Image;
+	public Button Button_Button;
 }
